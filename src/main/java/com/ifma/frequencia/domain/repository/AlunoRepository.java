7ba1d3b20560a07d327eedc9ac2b3dd9d52b9b03
David Gomesh <davidgomesh2016@gmail.com>
2023-02-05 15:38:56 -0300

    Corrigido Repositories

--- src/main/java/com/ifma/frequencia/domain/repository/AlunoRepository.java
+++ src/main/java/com/ifma/frequencia/domain/repository/AlunoRepository.java
@@ -1,12 +1,13 @@
 package com.ifma.frequencia.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ifma.frequencia.domain.model.Aluno;
-import com.ifma.frequencia.domain.repository.queries.AlunoQuery;
 
 @Repository
-public interface AlunoRepository extends JpaRepository<Aluno, Integer>, AlunoQuery {
-    
+public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
+    Optional<Aluno> findByCartao(String cartao);
 }
