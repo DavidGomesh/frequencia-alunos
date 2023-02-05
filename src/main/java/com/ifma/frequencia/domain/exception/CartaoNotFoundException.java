fc820eedc4fb1ecd314dc45f90e31e618111e926
David Gomesh <davidgomesh2016@gmail.com>
2023-02-05 15:35:57 -0300

    Corrigido Exceptions

--- src/main/java/com/ifma/frequencia/domain/exception/CartaoNotFoundException.java
+++ src/main/java/com/ifma/frequencia/domain/exception/CartaoNotFoundException.java
@@ -1,10 +1,8 @@
 package com.ifma.frequencia.domain.exception;
 
-import com.ifma.frequencia.domain.model.Cartao;
-
-public class CartaoNotFoundException extends EntityNotFoundException {
+public class CartaoNotFoundException extends RuntimeException {
 
     public CartaoNotFoundException(String msg) {
-        super(Cartao.class, msg);
+        super(msg);
     }
 }
